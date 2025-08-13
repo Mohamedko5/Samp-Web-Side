f26c629f12d71cd9037088c984c218680ee1d231
Mohamedko5 <mohamedkofli2001@gmail.com>
2025-08-14 02:02:14 +0800

    initial commit

--- lib/pages/body_pages.dart
+++ lib/pages/body_pages.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class BodyPages extends StatefulWidget {
+class BodyPages extends StatelessWidget {
   const BodyPages({super.key});
 
   @override
-  State<BodyPages> createState() => _BodyPagesState();
-}
-
-class _BodyPagesState extends State<BodyPages> {
-  bool isLeftMenuVisible = false; // Track if left menu is visible
-  bool isRightMenuVisible = false;
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Dashboard |",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 8),
+                Text("Student's Dashboard", style: TextStyle(fontSize: 15)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
