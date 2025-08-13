903bff43ea74d213f9743ba7843a66682487c37d
Mohamedko5 <mohamedkofli2001@gmail.com>
2025-08-13 23:10:09 +0800

    initial commit

---
+++ lib/pages/DashBoard_Drawer/Registration/course_reg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CourseReg extends StatefulWidget {
+  const CourseReg({super.key});
+
+  @override
+  State<CourseReg> createState() => _CourseRegState();
+}
+
+class _CourseRegState extends State<CourseReg> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("data")));
+  }
+}
